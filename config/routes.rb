a9ee0248a393ebb75fdb03c4b72dc47ffcad0fe1
rout666 <close.to.d@gmail.com>
2019-04-16 05:36:22 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
   get 'static_pages/contact'
 
   get 'static_pages/home'
@@ -9,5 +11,4 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  root 'application#hello'
 end
